f370a28c795344e679547031a6e48a585ca2b9d6
Nguyen Thi My Hanh <131949097+myhanhhhhh@users.noreply.github.com>
2023-07-04 18:53:48 +0700

    up bai tap

---
+++ src/ss1/practice/Time.java
@@ -0,0 +1,11 @@
+package ss1.practice;
+
+import java.util.Date;
+
+public class Time {
+
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is " + now);
+    }
+}
