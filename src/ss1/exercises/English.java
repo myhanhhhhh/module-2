c11db081ce4589abc2b9a046f3b312613119dd64
Nguyen Thi My Hanh <131949097+myhanhhhhh@users.noreply.github.com>
2023-07-04 18:37:27 +0700

    up bai tap

--- src/ss1/exercises/English.java
+++ src/ss1/exercises/English.java
@@ -4,91 +4,33 @@ import java.util.Scanner;
 
 public class English {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Nhap so vao day de kiem tra");
-        int number = scanner.nextInt();
-        String number1 = "one,two,three,four,10";
-        System.out.println(number1.charAt(1));
-        String number10 = "ten,two,three,four,20";
-        String number2 = "20, one,two,three,four,90";
+        System.out.println("Let enter number : ");
+        int number = Integer.parseInt(scanner.nextLine());
 
-//        switch (number) {
-//            case 0:
-//                System.out.println("zero");
-//                break;
-//            case 1:
-//                System.out.println("one");
-//                break;
-//            case 2:
-//                System.out.println("two");
-//                break;
-//            case 3:
-//                System.out.println("three");
-//                break;
-//            case 4:
-//                System.out.println("four");
-//                break;
-//            case 5:
-//                System.out.println("five");
-//                break;
-//            case 6:
-//                System.out.println("six");
-//                break;
-//            case 7:
-//                System.out.println("seven");
-//                break;
-//            case 8:
-//                System.out.println("eight");
-//                break;
-//            case 9:
-//                System.out.println("nine");
-//                break;
-//            default:
-//                System.out.println("Ngoai kieu du lieu. Vui long nhap lai ");
-//                break;
-//        }
-//        switch (number) {
-//            case 10:
-//                System.out.println("ten");
-//                break;
-//            case 11:
-//                System.out.println("eleven");
-//                break;
-//            case 12:
-//                System.out.println("twelve");
-//                break;
-//            case 13:
-//                System.out.println("thirteen");
-//                break;
-//            case 14:
-//                System.out.println("fourteen");
-//                break;
-//            case 15:
-//                System.out.println("fifteen");
-//                break;
-//            case 16:
-//                System.out.println("sixteen");
-//                break;
-//            case 17:
-//                System.out.println("seventeen");
-//                break;
-//            case 18:
-//                System.out.println("eighteen");
-//                break;
-//            case 19:
-//                System.out.println("nineteen");
-//                break;
-//            case 20:
-//                System.out.println("twenty");
-//                break;
-//        }
-//        if (number > 20 && number < 100) {
-//            if (number % 10 == 0) {
-//                System.out.println();
-//            }
-//
-//        }
-//    }
-}}
+        String[] arr1 = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String[] arr10 = {"ten", "eleven", "twelve", "thirdteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+        String[] arr100 = {"zero", "ten", "twenty", "thirdty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
 
+        if (number < 10) {
+            System.out.println(arr1[number]);
+        } else if (number < 20) {
+            System.out.println(arr10[number % 10]);
+        } else if (number < 100) {
+            System.out.println(arr100[number / 10] + arr1[number % 10]);
+        } else {
+
+            if ((number % 100) / 10 == 1) {
+                System.out.println(arr1[number / 100] + " hundred " + " and " + arr10[number % 10]);
+            } else if ((number % 100) / 10 == 0) {
+                System.out.println(arr1[number / 100] + " hundred " + " and " + arr1[number % 10]);
+            } else if ((number % 10 == 0)) {
+                System.out.println(arr1[number / 100] + " hundred " + " and " + arr100[(number % 100) / 10]);
+            } else {
+                System.out.println(arr1[number / 100] + " hundred " + " and " + arr100[(number % 100) / 10] + arr1[number % 10]);
+            }
+        }
+    }
+}
