1be0622002dcfc4af5a6a61d753bb68bd066dbda
Nguyen Thi My Hanh <131949097+myhanhhhhh@users.noreply.github.com>
2023-07-19 16:21:13 +0700

    up bai tap

---
+++ src/ss13/BinaryTree.java
@@ -0,0 +1,39 @@
+package ss13;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BinaryTree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> myList = new ArrayList<>();
+        System.out.println("nhap do dai mang");
+        int size = scanner.nextInt();
+        System.out.println("nhap so vao mang:");
+        for (int i = 0; i < size; i++) {
+            int number = scanner.nextInt();
+            myList.add(number);
+        }
+        System.out.println("mang vua tao:" + myList);
+        myList.sort(((o1, o2) -> o1 - o2));
+        System.out.println("mang sap xep tang dan: " + myList);
+        System.out.println("nhap so can tim:");
+        int number = scanner.nextInt();
+        System.out.println("so can tim o vi tri thu "+binarySearch( myList, 0, myList.size()-1, number));
+    }
+    public static int binarySearch(List<Integer> list, int left, int right, int value){
+        while(right>=left) {
+            int mid = (left+right)/2;
+            if(value < list.get(mid))
+                right = mid - 1;
+            else if (value == list.get(mid)) {
+                return mid;
+            }
+            else
+                left = mid + 1;
+        }
+        return -1;
+    }
+    }
+
--- src/ss13/ConsecutiveString.java
+++ src/ss13/ConsecutiveString.java
@@ -3,18 +3,20 @@ package ss13;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class AscendingString {
+public class ConsecutiveString {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("nhap tu vao day de kiem tra");
+        System.out.println("nhap chuoi vao kiem tra");
         String string = scanner.nextLine();
         LinkedList<Character> max = new LinkedList<>();
+        LinkedList<Character> list = new LinkedList<>();
         for (int i = 0; i < string.length(); i++) {
-            LinkedList<Character> list = new LinkedList<>();
             list.add(string.charAt(i));
             for (int j = i + 1; j < string.length(); j++) {
                 if (string.charAt(j) > list.getLast()) {
                     list.add(string.charAt(j));
+                } else {
+                    break;
                 }
             }
             if (list.size() > max.size()) {
@@ -24,7 +26,8 @@ public class AscendingString {
             list.clear();
         }
         for (Character element : max)
-        System.out.print(element);
+            System.out.print(element);
     }
 }
+
 
